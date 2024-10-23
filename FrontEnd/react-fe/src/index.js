aab09f01f4457a3c30442734ff3f67799f66d5cf
Tvisha Thakur <113552171+tvishathakur1@users.noreply.github.com>
2024-10-23 09:39:59 -0400

    Fixed the react DOM issue

--- FrontEnd/react-fe/src/index.js
+++ FrontEnd/react-fe/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import theme from '../src/utils/theme';
 import {ThemeProvider } from '@mui/material/styles';
-ReactDOM.render(
-    <ThemeProvider theme={theme}>
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>,
-  document.getElementById('root')
+    </ThemeProvider>
 );
+// ReactDOM.createRoot(
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>,
+//   document.getElementById('root')
+// );
 serviceWorker.unregister();
